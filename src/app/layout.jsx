b6aaac9d2c032f96a26907f02533df94921ba817
Shubham Dhiman <121211278+dhimanshubham440818@users.noreply.github.com>
2024-07-13 10:29:31 +0530

    Update layout.jsx
    removed font and linting

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/sectionCmp/Header/Header";
 import Footer from "@/components/sectionCmp/Footer/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Personal Portfolio",
@@ -14,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body className={inter.className}>
+      <body >
         {/* Header section  */}
         <Header />
 
